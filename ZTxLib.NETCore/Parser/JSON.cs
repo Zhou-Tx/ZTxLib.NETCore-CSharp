1b3032c21aae3cadf37156429e5d331190a0eef8
Zhou Tianxing <Zhou-Tx@users.noreply.github.com>
2019-09-09 17:25:48 +0800

    Update JSON

--- ZTxLib.NETCore/Parser/JSON.cs
+++ ZTxLib.NETCore/Parser/JSON.cs
@@ -6,29 +6,33 @@ namespace ZTxLib.NETCore.Parser
 {
     public class JSON
     {
-        private readonly string key;
         private readonly JToken json;
 
-        private JSON(JToken json, string key)
-        {
-            this.json = json;
-            this.key = key;
-        }
+        private JSON(JToken json) => this.json = json;
 
         public JSON(string json) => this.json = (JToken)JsonConvert.DeserializeObject(json);
 
-        public JSON this[string key] => new JSON(json[key], key);
+        public override string ToString() => json.ToString();
 
-        public override string ToString()
+        public JSON this[string key]
         {
-            try
+            get
             {
-                return json.ToString();
+                JToken jToken = json[key];
+                if (jToken == null)
+                    throw new NullReferenceException($"不存在该键值");
+                return new JSON(jToken);
             }
-            catch (NullReferenceException ex)
+        }
+
+        public JSON this[int index]
+        {
+            get
             {
-                string message = $"不存在键\"{key}\"";
-                throw new NullReferenceException(message, ex.InnerException);
+                JToken jToken = json[index];
+                if (jToken == null)
+                    throw new NullReferenceException($"不存在该对象");
+                return new JSON(jToken);
             }
         }
     }
